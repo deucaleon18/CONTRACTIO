3006275f9d6d8e9444d008306f5d64b6d4d88b96
deucaleon18 <shubham37908@gmail.com>
2022-03-13 08:40:45 +0530

    STYLING DONE

--- client/src/pages/ContractGov/ContractGov.js
+++ client/src/pages/ContractGov/ContractGov.js
@@ -118,21 +118,18 @@ const ContractGov = () => {
 
 
 
-
-
-
-
   return (
-    <div>
-    <h1>Contract-Details</h1>
+    <div className='container-main'>
+    <h1 className="detail-header">Contract-Details</h1>
+    <div className="detail-card">
 
-    <h1>NAME:{name}</h1>
-    <h1>DESC:{desc} </h1>
-    <h1>SECTOR:{sector}  </h1>
-    <h1>BASE:{baseAmount} </h1>
-    <h1>ALLOCATION:{allocation}   </h1>
-
-    <h1>BIDS:</h1>
+    <h1 className="detail-sides">NAME:{name}</h1>
+    <h1 className="detail-sides">DESC:{desc} </h1>
+    <h1 className="detail-sides">SECTOR:{sector}  </h1>
+    <h1 className="detail-sides">BASE:{baseAmount} </h1>
+    <h1 className="detail-sides">ALLOCATION:{allocation}   </h1>
+    </div>
+    <h1 className="detail-sides">BIDS:</h1>
 
     <div className='all-bids'>
      
@@ -143,7 +140,7 @@ const ContractGov = () => {
             <h1>DESCRIPTION:{bid.desc}</h1>
             <h1>AMOUNT:{bid.amount}</h1>
             <h1>BIDDER:{bid.bidder}</h1>
-            <button onClick={approveBid(bid.id)}>APPROVE BID</button>
+            <button className="general-button" onClick={approveBid(bid.id)}> APPROVE BID</button>
           </div>
           
           )
